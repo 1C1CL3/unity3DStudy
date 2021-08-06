d9cc8ef0a60eaf9a8552afcc36acfbdedf3c5973
1C1CL3 <48543154+1C1CL3@users.noreply.github.com>
2021-08-06 23:56:41 +0900

    8월 6일 452페이지 세이브 주석안달아놓음

---
+++ Assets/Script/EventRoot.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//여기에 event 클래스 추가
+
+public class Event
+{
+    public enum TYPE
+    {
+        NONE = -1,
+        ROCKET = 0,
+        NUM,
+    };
+};
+
+
+public class EventRoot : MonoBehaviour
+{
+    public Event.TYPE getEventType(GameObject event_go)
+    {
+        Event.TYPE type = Event.TYPE.NONE;
+        if (event_go != null)//인수에 게임 오브젝트가 비어있지 않으면
+        {
+            if(event_go.tag =="Rocket")
+            {
+                type = Event.TYPE.ROCKET;
+            }
+        }
+        return (type);
+    }
+    public bool isEventIgnitable(Item.TYPE carried_item, GameObject event_go)
+    {
+        bool ret = false;
+        Event.TYPE type = Event.TYPE.NONE;
+
+        if (event_go != null)
+        {
+            type = this.getEventType(event_go); // 이벤트 타입을 가져온다
+        }
+        switch (type)
+        {
+            case Event.TYPE.ROCKET:                 
+                if(carried_item == Item.TYPE.IRON)//가지고있는 것이 철광석이라면
+                {
+                    ret = true;
+                }
+                if(carried_item == Item.TYPE.PLANT)
+                {
+                    ret = true;
+                }
+                break;
+        }
+        return (ret);       
+    }
+
+
+}
